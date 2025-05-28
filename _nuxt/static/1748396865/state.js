7cb40bab789310703bfb7d10813ba6ef5b1d3bed
RuslanPykhov <ruslan.puhovv@gmail.com>
2025-05-28 05:06:44 +0300

    2

---
+++ _nuxt/static/1748396865/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvinoterrra\u002F_nuxt\u002Fstatic\u002F1748396865",layout:"default",error:a,state:{i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fvinoterrra\u002F",assetsPath:"\u002Fvinoterrra\u002F_nuxt\u002F",cdnURL:a}}}}(null));
